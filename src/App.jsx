d50e95947ca420ea880d1b2a89f6c06662be80f8
Andrew Starostin <andrewstarostinworkshop@gmail.com>
2025-12-15 20:29:54 +0400

    props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 // Homeworks
 import Homework_02 from "./homeworks/Homework_02/Homework_02";
 
+import Lesson_03 from "./lessons/lesson_03/lesson_03";
+
 function App() {
   return (
     <>
       {/* <Lesson_02 /> */}
-      <Homework_02/>
-
+      {/* <Homework_02/> */}
+      <Lesson_03 />
     </>
   );
 }
---
+++ src/lessons/lesson_03/lesson_03.jsx
@@ -0,0 +1,16 @@
+import Card from "../../homeworks/Homework_02/components/Card/Card";
+import "./styles.css"; 
+
+function Lesson_03() {
+  
+
+  return (
+    <div className="homework_02_wrapper">
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>
+  );
+}
+
+export default Lesson_03;
